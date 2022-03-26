cb0a82fb30b6f62be1ec8b0d52da481da6344885
zd9027 <zd9027@gmail.com>
2022-03-26 12:57:48 +0800

    -s

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "/20220324demo/",
+  outputDir: "docs",
+  productionSourceMap: false,
 });
